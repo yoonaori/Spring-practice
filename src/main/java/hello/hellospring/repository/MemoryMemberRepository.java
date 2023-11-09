807c7394a9041981a93389fc985ccc728ee477db
Park Yoon A <pya7106@naver.com>
2023-11-09 21:51:53 +0900

    spring
    1️⃣ 스프링 빈과 의존관계
    2️⃣ 회원 관리 예제 (웹 MVC 개발)

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -8,6 +9,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     private static Long sequence = 0L;
+
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
